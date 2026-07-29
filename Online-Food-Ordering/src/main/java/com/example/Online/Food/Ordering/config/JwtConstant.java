bbb098aafb86a505bd2cf679f544e9e68eff8806
Ivan <ivanbarbic13@gmail.com>
2026-07-29 16:39:21 +0200

    Add jwt dependecy working on confg and jwt validator

---
+++ Online-Food-Ordering/src/main/java/com/example/Online/Food/Ordering/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.example.Online.Food.Ordering.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "hasdjkgasdwqgheiwqgheqwigshkfjadgsakjdsahdqlowuwrqldhjslkadshlsadjsa";
+    public static final String JWT_HEADER = "Authorization";
+}
