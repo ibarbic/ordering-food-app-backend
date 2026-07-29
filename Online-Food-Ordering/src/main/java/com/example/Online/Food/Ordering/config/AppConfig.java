70586930ab0b268ab4c1aaccaf43ef399a6be22e
Ivan <ivanbarbic13@gmail.com>
2026-07-29 15:44:08 +0200

    Test

---
+++ Online-Food-Ordering/src/main/java/com/example/Online/Food/Ordering/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.example.Online.Food.Ordering.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+
+}
