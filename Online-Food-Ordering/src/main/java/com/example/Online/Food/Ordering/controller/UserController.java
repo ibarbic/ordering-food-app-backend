56004698ea8cc2755a6f751d9e8dedada9b5da70
Ivan <ivanbarbic13@gmail.com>
2026-08-04 14:05:10 +0200

    Adding user serivce interface and implemtations

---
+++ Online-Food-Ordering/src/main/java/com/example/Online/Food/Ordering/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.example.Online.Food.Ordering.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+//TODO: 3.28.26
+@RestController
+public class UserController {
+}
