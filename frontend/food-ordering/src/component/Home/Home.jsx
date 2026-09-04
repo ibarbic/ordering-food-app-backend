54aa19437380a3390934748c2f1c92ab846abccc
Ivan <ivanbarbic13@gmail.com>
2026-09-04 15:01:33 +0200

    finished home page

--- frontend/food-ordering/src/component/Home/Home.jsx
+++ frontend/food-ordering/src/component/Home/Home.jsx
@@ -1,9 +1,11 @@
+import RestaurantCard from "../Restaurant/RestaurantCard";
 import "./Home.css"
 import MultiItemCarousel from "./MultiItemCarousel";
 
+const restaurant = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 const Home = () => {
   return (
-    <div className=''>
+    <div className='pb-10'>
         <section className='banner -z-50 relative flex flex-col justify-center items-center'>
 
             <div className='w-[50vm] z-10 text-center'>
@@ -27,6 +29,19 @@ const Home = () => {
         <section className='p-10 lg:py-10 lg:px-20'>
             <p className='text-2xl font-semibold text-gray-400 py-3 pb-10'>Top Meals</p>
             <MultiItemCarousel/>
+        </section>
+
+        <section className='px-5 lg:px-20 pt-10'>
+            <h1 className='text-2xl font-semibold text-gray-400 pb-5'>
+                Order From Handpicked Favorites
+            </h1>
+            <div className ='flex flex-wrap items-center justify-around gap-5'>
+                {
+                    restaurant.map((item) => <RestaurantCard/>)
+                }
+
+            </div>
+
         </section>
         
     </div>
